f944d2d0d0764a53465303b3d0d8ea810eefe2b6
eVookelj <evukelj@stuy.edu>
2016-12-07 14:30:25 -0500

    Fix files

--- utils/tmEvents.py
+++ utils/tmEvents.py
@@ -1,14 +1,30 @@
 import urllib2
-from urllib import urlencode
-from os import urandom
-from hashlib import sha1, sha256
 import datetime
-import urllib2
 
-def tmTest():
-    apikey = "1"
-    url = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=%s"%(apikey)
-    u = urllib2.urlopen(url).read()
+query = "&size=20"
+apikey = "1"
+url = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=%s"%(apikey)
+
+def tmCall():
+    u = urllib2.urlopen(url+query).read()
     print u
+
+def tmKeyword(word):
+    global query
+    query += "&keyword=%s"(word)
+
+def tmCode(post):
+    global query
+    query += "&postalCode=%s"(post)
+
+def tmCity(city):
+    global query
+    query += "&city=%s"(city)
+
+#def tmStartTime(str):
+ #   global query
+  #  query += "&startDateTime
+#ALSO tmEndTime()
+
 
 tmTest()
