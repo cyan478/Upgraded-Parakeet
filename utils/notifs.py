15f9d32de6a60c764bce19862509514ef8ff6ec1
eVookelj <evukelj@stuy.edu>
2016-12-14 19:03:30 -0500

    Write basic notif fxns

--- utils/notifs.py
+++ utils/notifs.py
@@ -1,4 +1,5 @@
 import datetime
+from sqlite3 import connect
 
 f = "data/hangout.db"
 
@@ -8,16 +9,33 @@ def createNotif(user,msg):
     try:
         c.execute("SELECT * FROM notifs")
     except:
-        c.execute("CREATE TABLE notifs (username TEXT, message TEXT, datetime TEXT")
+        c.execute("CREATE TABLE notifs (username TEXT, message TEXT, datetime TEXT)")
     query = "INSERT INTO notifs VALUES (?, ?, ?)"
     c.execute(query, (user, msg, datetime.datetime.now()))
     db.commit()
     db.close()
 
 def getNotifs(user):
-    query = "SELECT msg FROM notifs WHERE username=? by datetime asc"
+    db = connect(f)
+    c = db.cursor()
+    query = "SELECT message FROM notifs WHERE username=? ORDER BY datetime asc"
     sel = c.execute(query, (user,))
     notifs = []
     for msg in sel:
         notifs.append(msg[0])
+    db.commit()
+    db.close()
     return notifs
+
+def removeNotif(user, msg):
+    db = connect(f)
+    c = db.cursor()
+    query = "DELETE FROM notifs WHERE username=? AND message=?"
+    c.execute(query, (user,msg))
+    db.commit()
+    db.close()
+
+#createNotif("emmaV", "glinda added you as a connect!")
+#print getNotifs("emmaV")
+#removeNotif("emmaV", "glinda added you as a connect!")
+#print getNotifs("emmaV")
